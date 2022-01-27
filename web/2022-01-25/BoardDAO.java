a6c2c1b97c831fde4764042debbb7a40672de148
MaybeYours <95428480+MaybeYours@users.noreply.github.com>
2022-01-28 00:43:49 +0900

    Update BoardDAO.java

--- web/2022-01-25/BoardDAO.java
+++ web/2022-01-25/BoardDAO.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;  
-import java.util.ArrayList;
+import java.util.ArrayList;    
+
 import java.util.List;
 
 import xyz.itwill.dto.BoardDTO;
