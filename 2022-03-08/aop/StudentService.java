09483a46f34f7e6e66ee7f962df56e8e37338e46
Ham_back_smile <95428480+MaybeYours@users.noreply.github.com>
2022-04-13 12:16:05 +0900

    Delete 2022-03-08 directory

--- 2022-03-08/aop/StudentService.java
+++
@@ -1,9 +0,0 @@
-package xyz.itwill07.aop;
-
-import java.util.List;
-
-public interface StudentService {
-	void addStudent(Student student);
-	Student getStudent(int num);
-	List<Student> getStudentList();
-}
